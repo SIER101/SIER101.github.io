4b329a15c74fc736b012ae202b8f159dccc6a904
73215103 <73215103+SIER101@users.noreply.github.com>
2023-11-18 12:27:20 +0700

    add 404

---
+++ src/pages/error404/error404.tsx
@@ -0,0 +1,9 @@
+function Error404() {
+  return (
+    <div>
+      <h1>404 Page not found</h1>
+    </div>
+  );
+}
+
+export default Error404;
--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,14 +4,16 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import About from "./about/about";
 import Home from "./home/home";
 import Projects from "./projects/projects";
+import Error404 from "./error404/error404";
 
 ReactDOM.createRoot(document.getElementById("main")!).render(
   <React.StrictMode>
     <HashRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/projects" element={<Projects />} />
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </HashRouter>
   </React.StrictMode>
